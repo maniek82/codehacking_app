d2f2df3f433642e08e60371ee7fd7589b1e54243
Mariusz <mariusz.tripler@gmail.com>
2016-09-03 11:00:36 +0000

    admin master page added project plus few style changes

--- gulpfile.js
+++ gulpfile.js
@@ -18,15 +18,15 @@ elixir(function(mix) {
         
         'libs/blog-post.css',
         'libs/bootstrap.css',
-        'libs/font-awsome.css',
+        'libs/font-awesome.css',
         'libs/metisMenu.css',
         'libs/sb-admin-2.css'
         
         ],'./public/css/libs.css')
         
         .scripts([
-            'libs/bootstrap.js',
             'libs/jquery.js',
+            'libs/bootstrap.js',
             'libs/metisMenu.js',
             'libs/sb-admin-2.js',
             'libs/scripts.js',
