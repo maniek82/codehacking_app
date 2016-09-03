91d2c04af599d3bb29c299d3e8a682ed75f0449f
Mariusz <mariusz.tripler@gmail.com>
2016-09-03 09:58:02 +0000

    after instalation of gulp and merging js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,29 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+    
+    .styles([
+        
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/font-awsome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css'
+        
+        ],'./public/css/libs.css')
+        
+        .scripts([
+            'libs/bootstrap.js',
+            'libs/jquery.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js',
+            'libs/custom.js',
+            
+            
+            
+            ],'./public/js/libs.js')
+    
+    
 });
