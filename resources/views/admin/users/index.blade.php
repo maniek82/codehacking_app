756cd9e621c6ad06b1a780174cb99769d488dec7
Mariusz <mariusz.tripler@gmail.com>
2016-09-03 11:22:57 +0000

    Added table to list all users in admin/users/index page and updated AdminUsersController to pull all users from database

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,31 @@
-<h1>INDEX PAGE</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+
+<h2>Users</h2>
+<table class="table table-hover table-bordered">
+    <thead >
+      <tr class="info">
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Created</th>
+        <th>Updated</th>
+      </tr>
+    </thead>
+    <tbody>
+        @if($users)
+        @foreach($users as $user)
+      <tr>
+        <td>{{$user->id}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{$user->created_at->diffForHumans()}}</td>
+        <td>{{$user->updated_at->diffForHumans()}}</td>
+      </tr>
+      @endforeach
+    @endif
+    </tbody>
+  </table>
+@endsection
